1c1437b367f6e14a49c3ce2d9e0ef1394b3037a0
unknown <jamie.g.ii@hotmail.com>
2025-02-26 18:24:11 -0600

    Got 5 random enemies added to the screen using a slice that I add new creatures to by using a for loop. Enemies overlap at the moment.

--- main.go
+++ main.go
@@ -2,38 +2,41 @@ package main
 
 import (
 	"fmt"
+	"math/rand/v2"
 
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
-type EnemyCreature struct {
+type PlayerCreature struct {
 	xpos  float32
 	ypos  float32
 	speed float32
 	size  float32
+	value float32 //point value for creature
 	color rl.Color
 }
 
-// New Creature containing values from EnemyCreature struct
-func NewCreature(xpos, ypos, speed, size float32, color rl.Color) EnemyCreature {
-	return EnemyCreature{
+// New Creature containing values from PlayerCreature struct
+func NewCreature(xpos, ypos, speed, size, value float32, color rl.Color) PlayerCreature {
+	return PlayerCreature{
 		xpos:  xpos,
 		ypos:  ypos,
 		speed: speed,
 		size:  size,
+		value: value,
 		color: color,
 	}
 }
 
-// function to draw creature from the EnemyCreature struct
-func (c EnemyCreature) DrawCreature() {
+// function to draw creature from the PlayerCreature struct
+func (c PlayerCreature) DrawCreature() {
 	rl.DrawRectangle(int32(c.xpos), int32(c.ypos), int32(c.size), int32(c.size), c.color)
 }
 
 // function to move the creature
-func (c *EnemyCreature) Move(xOffset, yOffset float32) {
-	c.xpos += xOffset * c.speed * rl.GetFrameTime()
-	c.ypos += yOffset * c.speed * rl.GetFrameTime()
+func (c *PlayerCreature) Move(xOffset, yOffset float32) {
+	c.xpos += xOffset //* c.speed * rl.GetFrameTime()
+	c.ypos += yOffset //* c.speed * rl.GetFrameTime()
 
 	//top and bottom boundaries
 	if c.ypos < 0 {
@@ -59,7 +62,14 @@ func main() {
 	rl.SetTargetFPS(60)
 
 	//initialize player creature outside of the game loop
-	player := NewCreature(100, 100, 200, 50, rl.Beige)
+	player := NewCreature(100, 100, 200, 50, 1, rl.Beige)
+
+	//slice to hold the enemy creatures
+	enemyCreatures := make([]PlayerCreature, 0)
+	//make 5 random creatures
+	for i := 0; i < 5; i++ {
+		enemyCreatures = append(enemyCreatures, NewCreature(float32(rand.IntN(rl.GetScreenWidth())), float32(rand.IntN(rl.GetScreenHeight())), float32(0), float32(50), float32(1), rl.Red))
+	}
 
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
@@ -69,21 +79,26 @@ func main() {
 		//draw player
 		player.DrawCreature()
 
+		//drawing the enemies
+		for _, enemy := range enemyCreatures {
+			enemy.DrawCreature()
+		}
+
 		//player movement
 		if rl.IsKeyPressed(rl.KeyW) {
-			player.Move(0, -10)
+			player.Move(0, -50)
 			fmt.Println("W Key Pressed")
 		}
 		if rl.IsKeyPressed(rl.KeyS) {
-			player.Move(0, 10)
+			player.Move(0, 50)
 			fmt.Println("S Key Pressed")
 		}
 		if rl.IsKeyPressed(rl.KeyA) {
-			player.Move(-10, 0)
+			player.Move(-50, 0)
 			fmt.Println("A Key Pressed")
 		}
 		if rl.IsKeyPressed(rl.KeyD) {
-			player.Move(10, 0)
+			player.Move(50, 0)
 			fmt.Println("D Key Pressed")
 		}
 		rl.EndDrawing()
